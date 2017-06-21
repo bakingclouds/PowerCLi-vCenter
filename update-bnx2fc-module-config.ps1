7352996e736190c4e9cff2af430cc0effa610fcf
Baking Clouds <info@bakingclouds.com>
2017-06-21 16:45:29 +1200

    Update update-bnx2fc-module-config.ps1

--- update-bnx2fc-module-config.ps1
+++ update-bnx2fc-module-config.ps1
@@ -8,15 +8,13 @@ Connect-VIServer $myvc
 #Get hosts with device 57840
 $my57840 = Get-VMHostPciDevice -DeviceClass NetworkController -Name *'QLogic 57840'*|Select VMHost -Unique
 foreach ($my57840s in $my57840){
-	If( (Get-VMHostModule -Name bnx2fc -VMHost $my57840.VMHost )|Where-Object {$_.Options -eq "bnx2fc_autodiscovery=1"} ) {
-Write-Host $my57840.VMHost 'module configuration not needed' -BackgroundColor Magenta
-Get-VMHostModule -Name bnx2fc -VMHost $my57840.VMHost |Select VMHost,Options
+	If( (Get-VMHostModule -Name bnx2fc -VMHost $my57840s.VMHost )|Where-Object {$_.Options -eq "bnx2fc_autodiscovery=1"} ) {
+Write-Host $my57840s.VMHost 'module configuration not needed' -BackgroundColor Magenta
 }
 Else{
-$esxcli = Get-EsxCli -VMHost $my57840.VMHost
+$esxcli = Get-EsxCli -VMHost $my57840s.VMHost
 $esxcli.system.module.parameters.set($false,"bnx2fc","bnx2fc_autodiscovery=1")
-Write-Host $my57840.VMHost 'Configured'
-Get-VMHostModule -Name bnx2fc -VMHost $my57840.VMHost |Select VMHost,Options
+Write-Host $my57840s.VMHost 'Configured'
 }
 	}
 
